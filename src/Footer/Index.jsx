6e02f547df5aaf925dbaec32fd3d8a886442c2fd
TH-montassar <montassar.themri@gmail.com>
2023-12-22 21:46:36 +0100

    setup gh-pages

--- src/Footer/Index.jsx
+++ src/Footer/Index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="footer rounded-lg shadow dark:bg-gray-900">
-      <div class="max-container mx-auto pb-4 md:pb-8">
-        <hr class="hr-line border-gray-200 sm:mx-auto dark:border-gray-700 lg:mb-8" />
-        <span class="text-info">
+    <footer className="footer">
+      <div className="max-container">
+        <hr className="hr-line" />
+        <span className="text-info">
           © 2023
           <a
             href="https://www.linkedin.com/in/montassar-front-end-dev/"
-            class="link hover:underline"
+            className="link"
           >
             Powred By MontassarThemri
           </a>
